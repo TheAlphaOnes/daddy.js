7f8484663b204aa7b97f4683b76a9cda418ac9a6
Vishnu-Norm <thenormvg@gmail.com>
2023-07-12 15:59:36 +0530

    created the what section

--- test/main.js
+++ test/main.js
@@ -1,6 +1,12 @@
 
-import { daddy } from "http://cdn.localhost:5000/daddy.js";
+// import { daddy } from "http://daddyjs.vercel.app/cdn/daddy.js";
 
-let data = {"12a":"gg1","12b":"gg2"}
-let alernate_data = [["gg1","alternate gg1"],["gg2","alternate gg2"]]
-daddy("https://daddyjstest.netlify.app",data,alernate_data)
+// let data = {"12a":"gg1","12b":"gg2"}
+// let alernate_data = [["gg1","alternate gg1"],["gg2","alternate gg2"]]
+// daddy("https://daddyjstest.netlify.app",data,alernate_data)
+
+import { daddy } from "http://daddyjs.vercel.app/cdn/daddy.js";
+
+let data = {"<parent website tag id>":"<child website tag id>","<parent website tag id>":"<child website tag id>"}
+let alernate_data = [["<child website tag id>","<alternate data>"],["<child website tag id>","<alternate data>"]]
+daddy("https://daddyjstest.test",data,alernate_data)
